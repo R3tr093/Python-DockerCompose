83b86d194d9e50a5bc857a081f6499aa46d4e15b
retro <mossiat.jeoffrey@outlook.com>
2019-11-28 10:20:30 +0100

    Initial commit

---
+++ client/client.py
@@ -0,0 +1,11 @@
+import urllib.request
+
+
+fp = urllib.request.urlopen("http://localhost:1234/")
+
+encodedContent = fp.read()
+decodedContent = encodedContent.decode("utf8")
+
+print(decodedContent)
+
+fp.close()
---
+++ server/server.py
@@ -0,0 +1,8 @@
+import http.server
+import socketserver
+
+handler = http.server.SimpleHTTPRequestHandler
+
+
+with socketserver.TCPServer(("",1234), handler) as httpd:
+    httpd.serve_forever()
